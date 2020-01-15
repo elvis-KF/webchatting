86269b1b127bb6aac900ed144cb8b62c41765cb5
elvis <553174325@qq.com>
2020-01-16 00:37:21 +0800

    feat: 修改样式

--- src/views/chatting/chatting.vue
+++ src/views/chatting/chatting.vue
@@ -5,25 +5,27 @@
       .chatting-user-info-box
         .chatting-user-avatar-box
           img(src="../../assets/default.jpg")
+        //- div(style="display: flex; justify-content: space-between; align-items: center;")
         .chatting-user-detail-box
           .chatting-user-name
             span 丘小峰
             img(src="../../assets/active.png")
-          .chatting-user-signature
-            | 少时诵诗书舒舒
+          //- .chatting-user-signature
+          //-   | 少时诵诗书舒舒
       .chatting-user-friends-box
-        .chatting-user-friends-item-box(v-for="item in friendList" @click="openChatWindow")
-          .chatting-user-avatar-box
-            img(src="../../assets/default.jpg")
-          .chatting-user-detail-box
-            .chatting-user-name
-              span {{item.name}}
-              template(v-if="item.avatar")
-                img(:src="item.avatar")
-              template(v-else)
-                img(src="../../assets/active.png")
-            .chatting-user-signature
-              | {{item.sign}}
+        .chatting-user-friends-item-box(v-for="(item, index) in friendList" :class="{ gray: index === currentClickIndex }" @click="openChatWindow(index)")
+          .chatting-friend-avatar-box
+            template(v-if="item.avatar")
+              img(:src="item.avatar")
+            template(v-else)
+              img(src="../../assets/default.jpg")
+          .chatting-friend-detail-box
+            .chatting-friend-info-box
+              .chatting-friend-name
+                span {{item.name}}
+              .chatting-friend-signature
+                | {{item.sign}}
+            img.status-img(src="../../assets/active.png")
     .chatting-window-box
       .chatting-
 </template>
@@ -37,6 +39,8 @@ import { Watch, Prop, Mixins } from "vue-property-decorator";
   components: {}
 })
 class Chatting extends Mixins(loader) {
+  currentClickIndex: string | number = '';
+
   friendList: Array<Object> = [
     {
       avatar: "",
@@ -45,8 +49,8 @@ class Chatting extends Mixins(loader) {
       status: 1 //0离线 1在线
     }
   ];
-  openChatWindow() {
-
+  openChatWindow(index: number) {
+    this.currentClickIndex = index;
   }
 }
 export default Chatting;
@@ -57,6 +61,9 @@ export default Chatting;
   height: 100vh;
   background-image: url(../../assets/background1.png);
   // opacity: .7;
+  .middle {
+    align-items: center;
+  }
   .chatting-content-box {
     display: flex;
     width: 1000px;
@@ -71,44 +78,53 @@ export default Chatting;
     .chatting-user-box {
       box-sizing: border-box;
       width: 320px;
-      // padding: 20px;
-      border-right: 1px solid #ced0d6;
+      background: #d0d0d0;
       .chatting-user-info-box {
+        width: 290px;
         display: flex;
         align-items: center;
-        height: 60px;
-        padding: 10px 15px;
-        border-bottom: 1px solid #ced0d6;
+        margin: 0 auto;
+        height: 55px;
+        padding: 10px 0;
+        border-bottom: 1px solid #979ba7;
+        .chatting-option-box {
+          img {
+            width: 23px;
+            height: 23px;
+          }
+        }
       }
-      .chatting-user-avatar-box {
+      .chatting-user-avatar-box{
         width: 50px;
         height: 50px;
         & > img {
           width: 100%;
           height: 100%;
+          border-radius: 50%;
         }
       }
       .chatting-user-detail-box {
-        width: 220px;
+        width: 230px;
         margin-left: 10px;
         text-align: left;
-        & > div {
-          width: 100%;
-          overflow: hidden;
-          white-space: nowrap;
-          text-overflow: ellipsis;
-        }
         .chatting-user-name {
-          width: 70%;
+          width: 100%;
+          display: flex;
+          justify-content: space-between;
           span, img {
             display: inline-block;
             vertical-align: middle;
           } 
           img {
-            width: 18px;
-            height: 18px;
-            margin-left: 8px;
+            width: 25px;
+            height: 25px;
             cursor: pointer;
+          }
+          span {
+            width: 80%;
+            overflow: hidden;
+            white-space: nowrap;
+            text-overflow: ellipsis;
           }
         }
         .chatting-user-signature {
@@ -122,14 +138,44 @@ export default Chatting;
       .chatting-user-friends-item-box{
         display: flex;
         padding: 15px;
+        font-size: 15px;
         &:hover {
           background: #e5e6e6;
+        }
+        .chatting-friend-detail-box {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          .status-img {
+            width: 25px;
+            height: 25px;
+          }
+        }
+        .chatting-friend-info-box {
+          width: 210px;
+          text-align: left;
+          padding-left: 10px;
+        }
+        .chatting-friend-avatar-box {
+          width: 44px;
+          height: 44px;
+          img {
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+          }
+        }
+        .chatting-friend-name {
+          margin-bottom: 5px;
         }
       }
     }
     .chatting-window-box {
       width: 680px;
     }
+  }
+  .gray {
+    background: #e5e6e6;
   }
 }
 </style>
